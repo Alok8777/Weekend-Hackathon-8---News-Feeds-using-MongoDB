4455c73c779edd610e12d9c5e3789cba29361c6a
Alok8777 <68750984+Alok8777@users.noreply.github.com>
2020-12-23 02:12:24 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -1,32 +1,49 @@
 const express = require("express");
+const { newsArticleModel } = require("./connector");
 const app = express();
 const port = 8080;
-const { newsArticleModel } = require("./connector");
-const { newsArticleSchema } = require("./schema");
+
 const onePageArticleCount = 10;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+let defLimit = 10,
+  defOffset = 0;
 app.get("/newFeeds", (req, res) => {
-  let limit = 10;
-  let offset = 0;
-  newsArticleModel.find({}, function (err, result) {
-    if (err) {
-      res.send(err);
-    } else {
-      let newRes = [];
-      for (let i = 0; i < limit; i++) {
-        newRes.push(result[i]);
-      }
-      for (let i = 0; i < offset; i++) {
-        newRes.pop();
-      }
-      console.log(newRes.length);
-      res.status(200).send(newRes);
+  let limitReceived = req.query.limit;
+  let offsetReceived = req.query.offset;
+  let limit, offset;
+  limit = parseInt(Number(limitReceived));
+  offset = parseInt(Number(offsetReceived));
+  if (!limitReceived) {
+    limit = defLimit;
+  }
+  if (!offsetReceived) {
+    offset = defOffset;
+  }
+  if (isNaN(offset)) {
+    offset = defOffset;
+  }
+  if (isNaN(limit)) {
+    limit = defLimit;
+    if (!offsetReceived) {
+      offset = defOffset;
     }
-  });
+  }
+  newsArticleModel
+    .find()
+    .then((result) => {
+      let start = 0;
+      start += offset;
+      let resultArray = [];
+      for (let startIndex = start; startIndex < start + limit; startIndex++) {
+        resultArray.push(result[startIndex]);
+      }
+      res.json(resultArray);
+    })
+    .catch((_) => res.json([]));
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
